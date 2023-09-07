e3c81c08edd9edf08e232dee152c8a98996c946d
sogol Malek <sogol.malek1@gmail.com>
2023-09-07 16:39:07 +0200

    update

--- ethereum-mpt-proof.js
+++ ethereum-mpt-proof.js
@@ -1,43 +1,39 @@
-const { Web3 } = require('web3');
-const Trie = require('merkle-patricia-tree');
-const util = require('ethereumjs-util');
+const ethers = require('ethers');
 
-// Replace with your Infura API key and Ethereum address
-const infuraApiKey = 'https://mainnet.infura.io/v3/72362f3c5a774276a3b19d84dfe57aee';
+// Replace with your Ethereum address
 const ethereumAddress = '0x9A4a5C159fB41F5AC3E4c7B5aBDCFB531a742d5c';
+const infuraApiKey = '72362f3c5a774276a3b19d84dfe57aee'; // Use your Infura API key
 
-// Create a Web3 instance connected to Infura
-const web3 = new Web3(`https://mainnet.infura.io/v3/72362f3c5a774276a3b19d84dfe57aee`);
-
-
-
+// Create an Ethereum provider using Infura
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${infuraApiKey}`);
 
 // Function to fetch MPT proof
 async function getMPTProof() {
-  const blockNumber = await web3.eth.getBlockNumber();
-  const transaction = await web3.eth.getTransactionFromBlock(blockNumber, 0); // Adjust the transaction index if needed
-  const receipt = await web3.eth.getTransactionReceipt(transaction.hash);
-  const proof = await web3.eth.getProof(receipt.blockNumber, [ethereumAddress], blockNumber);
+  const blockNumber = await provider.getBlockNumber();
+  const transaction = await provider.getTransactionByBlock(blockNumber, 0); // Adjust the transaction index if needed
+  const receipt = await provider.getTransactionReceipt(transaction.hash);
+  
+  // Use eth_getProof to get the MPT proof
+  const proof = await provider.send('eth_getProof', [ethereumAddress, [], blockNumber]);
+
   return proof;
 }
 
 // Function to verify MPT proof
-function verifyMPTProof(proof) {
-  const trie = new Trie();
-  trie.fromProof(proof.accountProof);
-  
-  const key = util.toBuffer(ethereumAddress);
-  const value = trie.get(key);
+async function verifyMPTProof(proof) {
+  const trie = new ethers.Trie(proof.root);
+  const key = ethereumAddress.toLowerCase();
+  const value = await trie.get(key);
 
   if (!value) {
     console.error('Account not found');
     return;
   }
 
-  const rootHash = util.toBuffer(proof.root);
+  const rootHash = proof.root;
   const expectedRootHash = trie.root;
 
-  if (rootHash.equals(expectedRootHash)) {
+  if (rootHash === expectedRootHash) {
     console.log('Proof verified successfully!');
   } else {
     console.error('Proof verification failed');
@@ -48,7 +44,7 @@ function verifyMPTProof(proof) {
 async function main() {
   try {
     const proof = await getMPTProof();
-    verifyMPTProof(proof);
+    await verifyMPTProof(proof);
   } catch (err) {
     console.error('Error:', err);
   }
