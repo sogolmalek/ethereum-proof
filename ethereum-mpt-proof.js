103db44770b89b373c4b499b95fab42f71ed4464
Luke Schoen <ltfschoen@gmail.com>
2023-09-09 12:10:44 +1000

    wip

--- ethereum-mpt-proof.js
+++ ethereum-mpt-proof.js
@@ -1,70 +1,84 @@
-const { Web3 } = require('web3');
-const Trie = require('merkle-patricia-tree');
-const util = require('ethereumjs-util');
+const { Web3 } = require("web3");
+const Trie = require("merkle-patricia-tree");
+const util = require("ethereumjs-util");
 const {BaseTrie} = require("merkle-patricia-tree");
 const {keccak256, toBuffer, Account, bufferToHex} = require("ethereumjs-util");
-// Replace with your Infura API key and Ethereum address
-const infuraApiKey = 'https://mainnet.infura.io/v3/72362f3c5a774276a3b19d84dfe57aee';
-const ethereumAddress = '0x9A4a5C159fB41F5AC3E4c7B5aBDCFB531a742d5c';
+import dotenv from "dotenv";
+dotenv.config();
 
-// Create a Web3 instance connected to Infura
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/72362f3c5a774276a3b19d84dfe57aee`));
-
-let block_to_verify = BigInt(0);
-
-// Function to fetch MPT proof
-async function getMPTProof() {
-  const blockNumber = await web3.eth.getBlockNumber();
-  block_to_verify = (blockNumber);
-  const transaction = await web3.eth.getTransactionFromBlock(blockNumber, 0); // Adjust the transaction index if needed
+// Function to fetch MPT proof for the given block number
+async function getMPTProof(publicKey, blockNumberToVerify) {
+  // Adjust the transaction index if needed
+  const transaction = await web3.eth.getTransactionFromBlock(blockNumberToVerify, 0);
   const receipt = await web3.eth.getTransactionReceipt(transaction.hash);
-  const proof = await web3.eth.getProof(ethereumAddress,   ["0x0000000000000000000000000000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000000000000000000000000001"], blockNumber);
+  const proof = await web3.eth.getProof(
+    // Address of account or contract
+    publicKey,
+    // Storage-keys to be proofed and included
+    [
+      "0x0000000000000000000000000000000000000000000000000000000000000000",
+      "0x0000000000000000000000000000000000000000000000000000000000000001"
+    ],
+    // TODO: Replace with with pre-defined block number "latest".
+    blockNumber
+  );
   return proof.accountProof;
 }
 
-// Function to verify MPT proof
-async function getproof(account_proof) {
-  
-  let result = await Trie.SecureTrie.fromProof(account_proof.map(util.toBuffer));
-  const accountNodeRaw = await result.get(keccak256(toBuffer(ethereumAddress)));
-  const account = Account.fromRlpSerializedAccount(accountNodeRaw);
-  return result;
-}
-
-// Main function
-async function main() {
-
-  try {
-    let proof_from_web3js = await getMPTProof()
-    
-    // done
-    let block_state = await readBlockHeader(block_to_verify)
-
-    let proof = await getproof(proof_from_web3js);
-    
-    console.log(" proof.root", proof.root)
-    
-    const valid = bufferToHex(proof.root) === block_state;
-    
-    if (valid){
-        console.log("Valid proof");
-    }else{
-        console.log("Invalid proof");
-    }
-  } catch (err) {
-    console.error('Error:', err);
-  }
-}
-
-
-
+// Function to read state root from the block header of a given block number
 async function readBlockHeader(blockNumber) {
   try {
-    let blockHeader =  await   web3.eth.getBlock(blockNumber)
-    console.log('blockHeader stateRoot:', blockHeader.stateRoot)
+    const blockHeader =  await web3.eth.getBlock(blockNumber);
+    console.log("blockHeader stateRoot:", blockHeader.stateRoot);
+
     return blockHeader.stateRoot;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
+
+// Function to verify that a transaction occurred using a given account for in the
+// given MPT proof state trie that is associated with a specific block
+async function getProof(proofBlock) {
+  let result = await Trie.SecureTrie.fromProof(proofBlock.map(util.toBuffer));
+  const accountNodeRaw = await result.get(keccak256(toBuffer(publicKey)));
+  const account = Account.fromRlpSerializedAccount(accountNodeRaw);
+  return account;
+}
+
+async function main() {
+  let providerURI = process.env.PROVIDER_URI;
+  // TODO: could provide API with multiple endpoints that allows user to specify a specific block number
+  // to be verified
+  let specificBlockNumberToVerify = BigInt(0);
+  const publicKey = process.env.ETHEREUM_ADDRESS_PUBLIC_KEY;
+
+  if (!providerURI || providerURI === "") {
+    // Fallback to locally running light client
+    providerURI = "http://127.0.0.1:8545";
+  }
+
+  // Create a Web3 instance connected to provider
+  const web3 = new Web3(new Web3.providers.HttpProvider(providerURI));
+
+  try {
+    // TODO: instead of just the current block, replace this with the latest block that has also been finalized
+    // similar to how it is done at this link https://github.com/ltfschoen/axiom-quickstart/blob/main/src/index.ts#L86
+    const blockNumberToVerify = await web3.eth.getBlockNumber() || specificBlockNumberToVerify;
+    let proofBlock = await getMPTProof(publicKey, blockNumberToVerify);
+    let blockStateRoot = await readBlockHeader(blockNumberToVerify);
+    let proof = await getProof(proofBlock);
+    console.log("proof.root", proof.root);
+    const valid = bufferToHex(proof.root) === blockStateRoot;
+
+    if (valid){
+      console.log("Valid proof");
+    }else{
+      console.log("Invalid proof");
+    }
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
 main();
